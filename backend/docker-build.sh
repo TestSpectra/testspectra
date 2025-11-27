996b22a1e3a5680a6604b3ccae5c681ea5959001
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-11-27 11:15:36 +0700

    refactor: consolidate backend into single service and switch to Alpine Linux
    - Remove user-service and grpc-proxy separation, merge into single testspectra-backend service
    - Switch base image from Debian to Alpine Linux for smaller image size
    - Update Dockerfile to build single binary instead of two separate services
    - Remove startup script that managed multiple processes
    - Simplify build process with single source directory and binary
    - Add musl-dev and openssl-static dependencies for Alpine buil

--- backend/docker-build.sh
+++
@@ -1,5 +0,0 @@
-# build user-service image
-docker build -f user-service/Dockerfile -t testspectra-user-service .
-
-# build grpc-proxy image
-docker build -f grpc-proxy/Dockerfile -t testspectra-grpc-proxy .
